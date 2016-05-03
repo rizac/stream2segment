7092495205bc05461a25e3ce02b25406761e79b2
rizac <riccardo.zaccarelli@gfz-potsdam.de>
2016-05-03 15:57:38 +0200

    implemented (not used) stream2segment.io.SessionScope

--- stream2segment/io/__init__.py
+++ stream2segment/io/__init__.py
@@ -1,19 +1,46 @@
-class Session(object):
+from contextlib import contextmanager
+from sqlalchemy.orm import Session
+from sqlalchemy.engine.base import Engine
+from sqlalchemy import create_engine
 
-    def __init__(self, sql_alchemy_engine):
-        self._engine = sql_alchemy_engine
 
-    def __enter__(self):
-        self._temporary_session = Session(self._engine)
-        return self.cr
+class SessionScope(object):
+    """
+        Class handling sqlalchemy sessions. Initialize this object with an sqlalchemy engine
+        and then 
+        Call self.session() to get an sqlalchemy session, or use a with statement which handles
+        commit and rollback when exiting:
+        with self.session_scope() as session:
+            ... do something with the session ...
+        NOTE: the with statement sets also the returned session as class attribute
+        self._open_session, so that subclasses might use it like this:
 
-    def __exit__(self, type_, value, traceback):
-        method_ = self._temporary_session.commit if \
-            value is None else self._temporary_session.rollback
+        def do_something(self, ...):
+            if self._open_session:
+    """
+    def __init__(self, sql_alchemy_engine_or_dburl):
+        if isinstance(sql_alchemy_engine_or_dburl, Engine):
+            self.engine = sql_alchemy_engine_or_dburl
+            self.db_uri = self.engine.engine.url
+        else:
+            self.db_uri = sql_alchemy_engine_or_dburl
+            self.engine = create_engine(sql_alchemy_engine_or_dburl)
+        self._open_session = None
+
+    @contextmanager
+    def session_scope(self):
+        """Provide a transactional scope around a series of operations."""
+        session = self.session()
+        self._open_session = session
         try:
-            method_()
+            yield session
+            session.commit()
         except:
-            try:
-                self._temporary_session.close()
-            except:
-                pass
+            session.rollback()
+            raise
+        finally:
+            session.close()
+            self._open_session = None
+
+    def session(self):
+        return Session(self.engine)
