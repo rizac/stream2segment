d2e9856f14b95542725aa10f4a3b0e2796bf961e
rizac <riccardo.zaccarelli@gmail.com>
2021-04-27 13:02:16 +0200

    testing new jupyter notebook WIP

--- tests/misc/test_notebook.py
+++ tests/misc/test_notebook.py
@@ -5,9 +5,10 @@ def test_notebook(data):
     import nbformat
     from nbconvert.preprocessors import ExecutePreprocessor
     # cwd = os.getcwd()
-    fle = get_resource_abspath('templates', 'jupyter.example.ipynb')
-    with open(fle) as f:
-        nb = nbformat.read(f, as_version=4)
-    ep = ExecutePreprocessor(timeout=600)  # , kernel_name='python3')
-    cwd = os.path.dirname(fle)
-    ep.preprocess(nb, {'metadata': {'path': cwd}})
+    for fle_ in [ 'jupyter.example.ipynb', 'the-segment-object.ipynb']:
+        fle = get_resource_abspath('templates', fle_)
+        with open(fle) as f:
+            nb = nbformat.read(f, as_version=4)
+        ep = ExecutePreprocessor(timeout=600)  # , kernel_name='python3')
+        cwd = os.path.dirname(fle)
+        ep.preprocess(nb, {'metadata': {'path': cwd}})
