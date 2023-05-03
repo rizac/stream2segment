1d45721e7ef198f5c567b0b7c394bf78027081bc
rizac <riccardo.zaccarelli@gmail.com>
2023-05-03 12:49:55 +0200

    fixing dstasts

--- stream2segment/download/db/inspection/static/js/leafletmap.js
+++ stream2segment/download/db/inspection/static/js/leafletmap.js
@@ -1,11 +1,11 @@
 function createLegend(map){
 	var legend = L.control({position: 'bottomleft'});
 	legend.onAdd = function (map) {
-		var div = L.DomUtil.create('div', 'info-legend');
-	    var child = document.getElementById('legend');
-	    child.parentNode.removeChild(child);
-	    div.appendChild(child);
-	    return div;
+		var div = L.DomUtil.create('div');
+		var child = document.getElementById('legend');
+		child.parentNode.removeChild(child);
+		div.appendChild(child);
+		return div;
 	};
 	legend.addTo(map);
 }
@@ -13,69 +13,30 @@ function createLegend(map){
 // create the options menu. This is in a function so that it can be called 
 // after the page has rendered AND SHOULD BE CALLED ONLY ONCE
 function createOptionsMenu(map){
-	var options = L.control({position: 'topright'});
+	var options = L.control({position: 'topright', collapsed: false});
 	options.onAdd = function (map) {
-	    var div = L.DomUtil.create('div', 'info-legend options');
-	    var child = document.getElementById('options');
-	    child.parentNode.removeChild(child);
-	    div.appendChild(child);
-	    // disable clicks and mouse wheel, so enable scrolling on the div:
-	    // https://github.com/dalbrx/Leaflet.ResizableControl/blob/master/src/Leaflet.ResizableControl.js#L98
-	    L.DomEvent.disableClickPropagation(child);  // for safety (is it needed?) 
-        L.DomEvent.on(child, 'mousewheel', L.DomEvent.stopPropagation);
-        
-        // add mouse out over effect (hide/ set visible like layers control)
-        var icon = L.DomUtil.create('div', 'options_icon');
-        icon.innerHTML = 'Options';
-        div.appendChild(icon);
-        var isOptionsDivVisible = true;
-        function setVisible(e, visible){
-        		if (visible != isOptionsDivVisible){
-        			child.style.display = visible ? 'block' : 'none';
-        			icon.style.display = visible ? 'none' : 'inline-block';
-        			isOptionsDivVisible = visible;
-	        		if (e){
-	        			L.DomEvent.stopPropagation(e);
-	        		}
-        		}
-        };
-        var showFunc = function(e){setVisible(e, true)};
-        var hideFunc = function(e){setVisible(e, false)};
-
-        // To use the options button as the leaflet control layer (using click event is more complex,
-        // as we should keep the icon div above visible, change its name to 'hide', and place it on the div):
-        L.DomEvent.on(div, 'mouseover', showFunc);
-        L.DomEvent.on(div, 'mouseout', hideFunc);
-		
-        // Set the default options div visibility:
-        setVisible(null, false);
-	    return div;
-	    //return child;
+		var div = L.DomUtil.create('div');
+		div.setAttribute('style', 'margin: 0px !important');
+		var child = document.getElementById('options');
+		//set height and margins:
+		child.style.margin = "10px 0";
+		child.style.maxHeight = 'calc(100vh - 20px)';
+		child.parentNode.removeChild(child);
+		div.appendChild(child);
+		// mouse wheel on the div should scroll it, not zoom the underlying map
+		// (Info here https://gis.stackexchange.com/a/111888):
+		L.DomEvent.on(div, 'mouseenter', () => { map.scrollWheelZoom.disable(); });
+		L.DomEvent.on(div, 'mouseleave', () => { map.scrollWheelZoom.enable(); });
+		return div;
 	};
 	options.addTo(map);
-	
 }
 
 function createMap(){
 	// creates the leaflet map, setting its initial bounds. Call map.getBounds() to return them
 	GLOBALS.map = map = new L.Map('map', {
-		scrollWheelZoom: false
+		// scrollWheelZoom: false
 	});
-	// initialize the map if not already init'ed
-	// provide two base layers. Keep it simple as many base layers are just to shof off
-	// and they do not need to be the main map concern
-	/*
-	// 1 MapBox Outdorrs (if you want more, sign in to mapbox. FIXME: why is it working with the token then?)
-	var geoportailLayer = L.tileLayer('https://wxs.ign.fr/{apikey}/geoportail/wmts?REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&STYLE={style}&TILEMATRIXSET=PM&FORMAT={format}&LAYER=ORTHOIMAGERY.ORTHOPHOTOS&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}', {
-		attribution: '<a target="_blank" href="https://www.geoportail.gouv.fr/">Geoportail France</a>',
-		bounds: [[-75, -180], [81, 180]],
-		minZoom: 2,
-		maxZoom: 19,
-		apikey: 'choisirgeoportail',
-		format: 'image/jpeg',
-		style: 'normal'
-	}).addTo(map);
-	*/
 	// 2 CartoDB gray scale map (very good with overlays, as in our case)
 	// the added base layer added is set selected by default (do not add the others then)
 	var cartoLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
@@ -83,13 +44,6 @@ function createMap(){
 		subdomains: 'abcd',
 		maxZoom: 19
 	}).addTo(map);
-/*
-	// instantiate a layer control (the button on the top-right corner for showing/hiding overlays
-	// overlays will be added when setting the tr model
-	var layersControl = L.control.layers({
-		'Map: Geoportail': geoportailLayer, 'Map: Carto': cartoLayer
-			}, {}, {collapsed: false, position: 'bottomleft'}).addTo(map);  // https://gis.stackexchange.com/a/68243
-*/
 	createLegend(map);
 	createOptionsMenu(map);
 	// create bounds and fit now, otherwise the map mathods latlng will not work:
@@ -134,12 +88,6 @@ function createMap(){
 		// console.log("ZOOMEND", e);
 		updateMap();
 	});
-	// moveend has a problem: it might be moved when showing a popup
-	// in this case the map is updated and the popup venishes...
-//	map.on("moveend", function (e) {
-//		// console.log("ZOOMEND", e);
-//		updateMap();
-//	});
 	return map;
 }
 
@@ -161,8 +109,6 @@ function _updateMap(){
 	 * which are much more lightweight than svgicons but with sizes relative to the map coordinates,
 	 * thus zoom dependent, whereas we want them zoom independent
 	*/
-
-	// var {datacenters, seldatacenters, networks, codes, selcodes, downloads, seldownloads} = GLOBALS;
 
 	var attr = 'data-seldownload-id';
 	var seldownloads = new Set(
@@ -260,8 +206,8 @@ function _updateMap(){
 	for (var dcId in GLOBALS.datacenters){  // iterate over Object keys (datacenter id)
 		var {ok, total} = (dcId in dcStats) ? dcStats[dcId] : {'ok': 0, 'total': 0};
 		//update stats in the dropdown menu Options:
-		htmlElement(`dc${dcId}total`).innerHTML = total;
-		htmlElement(`dc${dcId}sel`).innerHTML = ok;
+		htmlElement(`dc${dcId}total`).innerHTML = total.toLocaleString('en-US');
+		htmlElement(`dc${dcId}sel`).innerHTML = ok.toLocaleString('en-US');
 		htmlElement(`dc${dcId}selperc`).innerHTML = `${total ? Math.round((100*ok)/total) : 0}%`;
 	}
 	// set the mapLayer so that we will know what to clear the next time we call this method:
@@ -324,24 +270,24 @@ function createMarker(staName, netName, staId, latLng, dcId, datacenter, ok, mal
 	var xx = Math.abs(latlng2.lng - lon);
 	var yy = Math.abs(latlng2.lat - lat);
 	
-    // zIndexOffset is an OFFSET. If made in thousands it basically works as a zIndex
+	// zIndexOffset is an OFFSET. If made in thousands it basically works as a zIndex
 	// (see last post here: https://github.com/Leaflet/Leaflet/issues/5560)
 	// it is used only if Markers are supplied. If Polygon (as in this case) they will be used
 	// for ordering the markers
-    var zIndexOffset = (val > 0 ? 10000 : 0) + 100 * size;
-    var latlngs = [[lat-yy/2, lon-xx],[lat+yy, lon], [lat-yy/2, lon+xx]];
-    var tri = L.polygon(latlngs, {fillOpacity: 1, color: '#333', fillColor:`rgb(255, ${greenBlue}, ${greenBlue})`,
-    	weight:1, zIndexOffset: zIndexOffset});
+	var zIndexOffset = (val > 0 ? 10000 : 0) + 100 * size;
+	var latlngs = [[lat-yy/2, lon-xx],[lat+yy, lon], [lat-yy/2, lon+xx]];
+	var tri = L.polygon(latlngs, {fillOpacity: 1, color: '#333', fillColor:`rgb(255, ${greenBlue}, ${greenBlue})`,
+		weight:1, zIndexOffset: zIndexOffset});
 	
 	//bind popup with infos:
-	var staPopupContent = `<div class='title'> ${staName}.${netName} </div>
-						   <div class='subtitle underline'>${datacenter}</div>
-						   <table class='station-info'>
-						   <tr><td>database id:</td><td class='right'>${staId}</td></tr>
-						   <tr><td>Segments:</td><td class='right'> ${total} </td></tr>
-						   <tr><td class='right'>In selected categories:</td><td class='right'> ${ok} </td></tr>
-						   <tr><td class='right'>Not in selected categories:</td><td class='right'> ${malformed} </td></tr>
-						   </table>`; 
+	var staPopupContent = `<h5> ${staName}.${netName} </h5>
+							<div>${datacenter}</div>
+							<table class='table'>
+							<tr><td>database id:</td><td class='text-end'>${staId}</td></tr>
+							<tr><td>Segments:</td><td class='text-end'> ${total.toLocaleString('en-US')} </td></tr>
+							<tr><td>In selected categories:</td><td class='text-end'> ${ok.toLocaleString('en-US')} </td></tr>
+							<tr><td>Not in selected categories:</td><td class='text-end'> ${malformed.toLocaleString('en-US')} </td></tr>
+							</table>`;
 	tri.bindPopup(staPopupContent);
 
 	return tri;
